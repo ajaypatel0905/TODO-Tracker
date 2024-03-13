3a98f0598deff0ae8ababdb5d3a2b6f0b40361be
js141199 <jeet.shah@students.iiit.ac.in>
2024-03-13 13:01:46 +0530

    Changed folder structure and model files.

--- src/main/java/org/TODOTracker/model/Task.java
+++ src/main/java/org/TODOTracker/model/Task.java
@@ -9,12 +9,12 @@ import java.lang.*;
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long taskId;
+    private Long id;
 
     private String title;
     private String description;
@@ -30,6 +30,61 @@ public class Task {
     @JoinColumn(name = "user_id")
     private User user;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public DateType getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(DateType deadline) {
+        this.deadline = deadline;
+    }
+
+    public DateType getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(DateType createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Boolean getExist() {
+        return isExist;
+    }
+
+    public void setExist(Boolean exist) {
+        isExist = exist;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public String toString() {
         return "Task{" +
--- src/main/java/org/TODOTracker/model/User.java
+++ src/main/java/org/TODOTracker/model/User.java
@@ -9,12 +9,12 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
 
     private String emailId;
 
@@ -25,6 +25,46 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Task> tasks;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getExist() {
+        return isExist;
+    }
+
+    public void setExist(Boolean exist) {
+        isExist = exist;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 
     public String toString() {
         return "User{" +
